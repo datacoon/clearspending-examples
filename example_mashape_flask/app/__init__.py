6912eba2fc10e2c6011f23a5516f84b946200e1d
Dmitry <dmitryrudneff@gmail.com>
2015-10-30 11:18:47 +0300

    Deleted unusable examples.

--- example_mashape_flask/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-flask_app = Flask(__name__)
-
-from app import views
--- example_mashape_flask/run.py
+++
@@ -1,3 +0,0 @@
-#!flask/bin/python
-from app import flask_app
-flask_app.run(host='localhost', debug=True)
